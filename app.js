b137dddf679350d66f1e66d0baf3166f1d3f31df
magdalena-Nowak <magdalena.nowak2705@gmail.com>
2021-09-03 11:00:16 +0200

    inputs fill checking

--- app.js
+++ app.js
@@ -7,15 +7,49 @@ const clearBtn = document.querySelector(".form__btn-clear");
 const submitBtn = document.querySelector(".form__btn-submit");
 const confirmPopup = document.querySelector(".popup");
 const confirmBtn = document.querySelector(".popup__btn");
-const allInputs = document.querySelectorAll("input");
+let allInputs;
+
+const showErr = (input, message) => {
+  input.classList.add("alert-warning");
+  input.nextElementSibling.classList.add("error");
+  input.nextElementSibling.textContent = message;
+};
 
 const clearInput = (input) => {
   input.value = "";
+  input.nextElementSibling.classList.remove("error");
+  input.classList.remove("alert-warning");
+};
+
+const confirmSending = () => {
+  confirmPopup.classList.add("active");
+};
+
+const checkForm = (input) => {
+  if (input.value === "") {
+    showErr(input, input.placeholder);
+  } else {
+    clearInput(input);
+    confirmSending();
+  }
 };
 
 clearBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  allInputs = document.querySelectorAll("input");
   allInputs.forEach((input) => {
     clearInput(input);
   });
 });
 
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  allInputs = document.querySelectorAll("input");
+  allInputs.forEach((input) => {
+    checkForm(input);
+  });
+});
+
+confirmBtn.addEventListener('click', () => {
+  confirmPopup.classList.remove("active");
+})
