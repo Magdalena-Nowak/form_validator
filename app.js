11be3cb6c2eb67a71e45faf0a5272a4677ccc794
magdalena-Nowak <magdalena.nowak2705@gmail.com>
2021-09-03 15:42:58 +0200

    finish checking form

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const userName = document.querySelector("#username");
 const pass1 = document.querySelector("#password1");
 const pass2 = document.querySelector("#password2");
+const userEmail = document.querySelector("#email");
 const clearBtn = document.querySelector(".form__btn-clear");
 const submitBtn = document.querySelector(".form__btn-submit");
 const confirmPopup = document.querySelector(".popup");
 const confirmBtn = document.querySelector(".popup__btn");
-let userEmail;
 let min = 4;
 let allInputs;
 
@@ -22,21 +22,25 @@ const clearInput = (input) => {
 };
 
 const checkPassStrength = (input) => {
-  const passRegex =
-    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})";
-  if (passRegex.test(input)) {
-    console.log("ok");
+  const passRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+  if (passRegex.test(input.value)) {
+    input.nextElementSibling.classList.remove("error");
+    console.log(`Hasło pasuje: ${input.value}`);
   } else {
+    console.log(`Hasło nie pasuje: ${input.value}`);
     showErr(
       input,
-      "Hasło musi zawierać minimum 8 znaków w tym przynajmniej jedną: wielką litere, małą litere, cyfre, znak specjalny"
+      "Hasło musi zawierać minimum 8 znaków w tym przynajmniej jedną: wielką literę, małą literę oraz cyfrę"
     );
   }
 };
 
 const checkPassword = (pass1, pass2) => {
   if (pass1.value !== pass2.value) {
+    console.log(`Pass1: ${pass1.value}, pass2: ${pass2.value}`);
     showErr(pass2, "Hasła nie pasują do siebie");
+  } else {
+    pass2.nextElementSibling.classList.remove("error");
   }
 };
 
@@ -45,29 +49,40 @@ const checkLength = (input, min) => {
     const inputAttribute = input.getAttribute("data-type");
     errMessage = `${inputAttribute} składa się z min. ${min} znaków`;
     showErr(input, errMessage);
+  } else {
+    input.nextElementSibling.classList.remove("error");
   }
 };
 
-const checkMail = (email) => {
+const checkEmail = (input) => {
   const emailRegex =
-    '/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
-  if (emailRegex.test(email.value)) {
-    clearInput(email);
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  console.log(`Email: ${input.value}`);
+  if (emailRegex.test(input.value)) {
+    input.nextElementSibling.classList.remove("error");
   } else {
-    showErr(email, "E-mail jest niepoprawny");
+    showErr(input, "E-mail jest niepoprawny");
   }
 };
 
 const checkForm = (input) => {
-  if (input.value === "") {
-    showErr(input, input.placeholder);
-  } else {
-    checkLength(userName, min);
-    checkPassStrength(pass1);
-    checkPassword(pass1, pass2);
-    checkMail(userEmail);
-    checkErrors();
-    console.log(pass1, pass2);
+  input.forEach((input) => {
+    if (input.value === "") {
+      showErr(input, input.placeholder);
+    }
+  });
+};
+
+const checkErrors = () => {
+  const allInputs = document.querySelectorAll("input");
+  let errorCount = 0;
+  allInputs.forEach((input) => {
+    if (input.nextElementSibling.classList.contains("error")) {
+      errorCount++;
+    }
+  });
+  if (errorCount === 0) {
+    confirmPopup.classList.add("active");
   }
 };
 
@@ -81,8 +96,14 @@ clearBtn.addEventListener("click", (e) => {
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  allInputs = document.querySelectorAll("input");
-  allInputs.forEach((input) => {
-    checkForm(input);
-  });
+  checkForm([userName, pass1, pass2, userEmail]);
+  checkLength(userName, min);
+  checkPassStrength(pass1);
+  checkPassword(pass1, pass2);
+  checkEmail(userEmail);
+  checkErrors();
+});
+
+confirmBtn.addEventListener("click", () => {
+  confirmPopup.classList.remove("active");
 });
